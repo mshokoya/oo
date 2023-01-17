868043a356c6cc1a3afae73630a0ed7224869ad0
mshokoya <mayo_s@hotmail.co.uk>
2023-01-17 01:24:43 +0000

    testa10s

--- users/cmd/main.go
+++ users/cmd/main.go
@@ -18,7 +18,7 @@ func main() {
 
 	flag.Parse()
 
-	fmt.Println("test dsdsdsdsdddsscdscdsdsdsaddassdsadsadddsasdsaadsadsdasad3")
+	fmt.Println("test dsdsdsdsdddsscdscdsdsdsaddasdadasdasasdsadsadddsasdsaadsadsdasad3")
 
 	err := config.LoadEnvVars(*envPath, &env)
 	if err != nil {
