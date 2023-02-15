dedee618c62e599414f7349f1d22ad338d238192
mshokoya <mayo_s@hotmail.co.uk>
2023-02-15 21:50:39 +0000

    works

--- users/internal/application/user_handler.go
+++ users/internal/application/user_handler.go
@@ -56,3 +56,54 @@ func (app *Application) RegisterUserHandler(w http.ResponseWriter, r *http.Reque
 		app.ServerErrorResponse(w,r, err)
 	}
 }
+
+func (app *Application) UpdateUserHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Username string `json:"username"`
+		Firstname string `json:"firstname"`
+		Lastname string `json:"lastname"`
+	}
+
+	err := app.ReadJSON(w, r, &input)
+	if err != nil {
+		app.BadRequestResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+	user := repository.User{}
+
+	if input.Username != "" {
+		ValidateUsername(v, input.Username)
+		user.Username = input.Username
+	}
+
+	if input.Firstname != "" {
+		v.Check(len(input.Firstname) >= 2, "firstname", "Invalid firstname provided")
+		user.Firstname = input.Firstname
+	}
+
+	if input.Lastname != "" {
+		v.Check(len(input.Lastname) >= 2, "lastname", "Invalid lastname provided")
+		user.Lastname = input.Lastname
+	}
+
+	if !v.Valid() {
+		app.FailedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	err = app.Models.Users.Update(user)
+	if err != nil {
+		app.ServerErrorResponse(w, r, err)
+		return
+	}
+
+	env := envelope{"message": "your details was successfully updated"}
+
+	err = app.WriteJSON(w,http.StatusAccepted, env, nil)
+	if err != nil {
+		app.ServerErrorResponse(w, r, err)
+		return
+	}
+}
